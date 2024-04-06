322e6e3d44e2b35475fef61e6aa337163c458a95
avi-lek <91644257+avi-lek@users.noreply.github.com>
2024-04-06 13:03:31 -0700

    Update sandbox_functions.py

--- sandbox_functions.py
+++ sandbox_functions.py
@@ -14,10 +14,10 @@ import pandas as pd
 # gets PDB content
 def get_pdb_content():
     if st.session_state["use_esm"]:
-       get_esm_pdb2(st.session_state["esm_seq"], ("sandbox_new"))
+       get_esm_pdb(st.session_state["esm_seq"], ("sandbox_new"))
        st.write(st.session_state["esm_seq"])
        #if bool(~os.path.isfile("pdb/sandbox.pdb")): get_esm_pdb(st.session_state["esm_seq"], ("sandbox"))
-       with open("pdb/sandbox_new.pdb") as ifile:
+       with open("pdb\\sandbox_new.pdb") as ifile:
            system = "".join([x for x in ifile])
            st.caption("Source: " + system.split('\n')[1][6:])
            return system
