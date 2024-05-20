dbc2f39171441a80c5784fd05bd5f8764ee872f8
avi-lek <avi.lekkelapudi@gmail.com>
2024-05-20 10:02:46 -0700

    asd

--- Home.py
+++ Home.py
@@ -18,6 +18,46 @@ st.markdown("""
         """, unsafe_allow_html=True)
 show_pages_from_config()
 
+ms = st.session_state
+if "themes" not in ms: 
+  ms.themes = {"current_theme": "light",
+                    "refreshed": True,
+                    
+                    "light": {"theme.base": "dark",
+                              "theme.backgroundColor": "#0E1117",
+                              "theme.primaryColor": "#FF4B4B",
+                              "theme.secondaryBackgroundColor": "#262730",
+                              "theme.textColor": "#FAFAFA",
+                              "button_face": "🌜"},
+
+                    "dark":  {"theme.base": "light",
+                              "theme.backgroundColor": "#FFFFFF",
+                              "theme.primaryColor": "#FF4B4B",
+                              "theme.secondaryBackgroundColor": "#F0F2F6",
+                              "theme.textColor": "#31333F",
+                              "button_face": "🌞"},
+                    }
+  
+
+def ChangeTheme():
+    previous_theme = ms.themes["current_theme"]
+    tdict = ms.themes["light"] if ms.themes["current_theme"] == "light" else ms.themes["dark"]
+    for vkey, vval in tdict.items(): 
+        if vkey.startswith("theme"): st._config.set_option(vkey, vval)
+
+    ms.themes["refreshed"] = False
+    if previous_theme == "dark": ms.themes["current_theme"] = "light"
+    elif previous_theme == "light": ms.themes["current_theme"] = "dark"
+
+btn_face = ms.themes["light"]["button_face"] if ms.themes["current_theme"] == "light" else ms.themes["dark"]["button_face"]
+
+st.sidebar.info("Changing to Light/Dark Mode may not work initially. If so, reload page.")
+st.sidebar.toggle("Light/Dark Mode", on_change=ChangeTheme)
+
+if ms.themes["refreshed"] == False:
+  ms.themes["refreshed"] = True
+  st.rerun()
+
 # Write Instructions
 url_feedback = "https://forms.gle/q9mjvDEk1PeHW5aTA"
 st.markdown('''After using this website, please let us know how you liked it [here](%s)!''' % url_feedback)
